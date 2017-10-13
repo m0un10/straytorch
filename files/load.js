3984c5cc0e9d0301c11c76204b2687ac7fabef7d
m0un10 <cbarr.aus@gmail.com>
2017-10-13 10:51:07 +1000

    report is now completely generic

--- files/load.js
+++ files/load.js
@@ -2,10 +2,10 @@ const grid = new tui.Grid({
     el: document.getElementById('grid'),
     data: data,
     rowHeaders: ['rowNum'],
-    bodyHeight: 500,
+    bodyHeight: gridBodyHeight,
     summary: {
       height: 40,
-      position: 'bottom', // or 'top'
+      position: 'bottom',
       columnContent: {
         'key': {
           template: function(valueMap) {
@@ -22,133 +22,5 @@ const grid = new tui.Grid({
     columnOptions: {
       resizable: true
     },
-    columns: [
-        {
-            header: 'Name',
-            name: 'key',
-            filter: { 
-              type: 'text',
-              showClearBtn: true
-            },
-            ellipsis: true
-        },
-        {
-          header: 'Difference?',
-          name: 'difference',
-          width: 100,
-          align: 'center',
-          resizable: false,
-          filter: 'select'
-        },
-        {
-            header: 'acg.r01.ana.soa',
-            name: 'acg.r01.ana.soa',
-            align: 'center',
-            ellipsis: true,
-            filter: { 
-              type: 'text',
-              showClearBtn: true
-            }
-        },
-        {
-            header: 'acg.r01.dit.soa',
-            name: 'acg.r01.dit.soa',
-            align: 'center',
-            ellipsis: true,
-            filter: { 
-              type: 'text',
-              showClearBtn: true
-            }
-        },
-        {
-          header: 'acg.r01.bld.soa',
-          name: 'acg.r01.bld.soa',
-          align: 'center',
-          ellipsis: true,
-          filter: { 
-            type: 'text',
-            showClearBtn: true
-          }
-        },
-        {
-          header: 'acg.r01.drd.soa',
-          name: 'acg.r01.drd.soa',
-          align: 'center',
-          ellipsis: true,
-          filter: { 
-            type: 'text',
-            showClearBtn: true
-          }
-        },
-        {
-          header: 'acg.r01.trn.soa',
-          name: 'acg.r01.trn.soa',
-          align: 'center',
-          ellipsis: true,
-          filter: { 
-            type: 'text',
-            showClearBtn: true
-          }
-        },
-        // {
-        //   header: 'acg.r99.upg.soa',
-        //   name: 'acg.r99.upg.soa',
-        //   align: 'center',
-        //   ellipsis: true,
-        //   filter: { 
-        //     type: 'text',
-        //     showClearBtn: true
-        //   }
-        // },
-        {
-          header: 'acg.r01.eft.soa',
-          name: 'acg.r01.eft.soa',
-          align: 'center',
-          ellipsis: true,
-          filter: { 
-            type: 'text',
-            showClearBtn: true
-          }
-        },
-        {
-          header: 'acg.r01.sit.soa',
-          name: 'acg.r01.sit.soa',
-          align: 'center',
-          ellipsis: true,
-          filter: { 
-            type: 'text',
-            showClearBtn: true
-          }
-        },
-        {
-          header: 'acg.r01.cit.soa',
-          name: 'acg.r01.cit.soa',
-          align: 'center',
-          ellipsis: true,
-          filter: { 
-            type: 'text',
-            showClearBtn: true
-          }
-        },
-        {
-          header: 'acg.r01.spt.soa',
-          name: 'acg.r01.spt.soa',
-          align: 'center',
-          ellipsis: true,
-          filter: { 
-            type: 'text',
-            showClearBtn: true
-          }
-        },
-        {
-          header: 'acg.r01.pst.soa',
-          name: 'acg.r01.pst.soa',
-          align: 'center',
-          ellipsis: true,
-          filter: { 
-            type: 'text',
-            showClearBtn: true
-          }
-        }
-    ]
-    });
+    columns: columns
+});
--- straytorch.py
+++ straytorch.py
@@ -6,18 +6,33 @@ import subprocess
 import datetime
 from os.path import expanduser
 from reports import fine
+import argparse
+import sys
+import yaml
+import re
+
+parser = argparse.ArgumentParser(description='Discover deep insights into multi-instance data differences.')
+parser.add_argument('-c', '--config', help='location of configuration')
+
+args = parser.parse_args()
+
+if args.config is None:
+  parser.print_help()
+  sys.exit(1)
 
 home = os.environ['STRAYTORCH_HOME']
 fallback_output_home = expanduser("~") + "/.straytorch"
 
-report_config = {
-    'report': {
-        'name': 'Environment Difference Report',
-        'autoTimestamp': True,
-        'outputDirectory': 'output',
-        'type': 'fine',
-    },
-}
+if args.config.endswith(".yaml") or args.config.endswith(".yml"):
+  with open(args.config, 'r') as stream:
+      try:
+          report_config = yaml.safe_load(stream)
+      except yaml.YAMLError as exc:
+        print("invalid yaml in "+args.config+ ": "+str(exc))
+        sys.exit(1)
+else:
+  print("unsupported file type: "+args.config)
+  sys.exit(1)
 
 def flatten_json(y):
     out = {}
@@ -39,8 +54,39 @@ source_data = {}
 
 number_of_columns = 0
 
+columns_data = [
+  {
+      'header': 'Name',
+      'name': 'key',
+      'filter': { 
+        'type': 'text',
+        'showClearBtn': True
+      },
+      'ellipsis': True
+  },
+  {
+    'header': 'Difference?',
+    'name': 'difference',
+    'width': 100,
+    'align': 'center',
+    'resizable': False,
+    'filter': 'select'
+  }
+]
+
 for filename in os.listdir('.'):
-  if filename.endswith(".json"):
+  match = re.match('(?P<name>.*).json', filename)
+  if match:
+    columns_data.append({
+      'header': match.group('name'),
+      'name':  match.group('name'),
+      'align': 'center',
+      'ellipsis': True,
+      'filter': { 
+        'type': 'text',
+        'showClearBtn': True
+      }
+    })
     with open(filename) as f:
       data = json.load(f)
       source_data[os.path.splitext(filename)[0]] = flatten_json(data)
@@ -60,7 +106,12 @@ if report_type == "fine":
   sorted_data = sorted(fine.report(data,number_of_columns), key=lambda k: k['key'].lower())
 else:
   print(report_type + " is not a supported report type.")
-  os.exit(1)
+  sys.exit(1)
+
+if not 'grid' in report_config:
+  report_config['grid'] = {}
+if not 'bodyHeight' in report_config['grid']:
+  report_config['grid']['bodyHeight'] = 'auto'
 
 if "outputDirectory" in report_config['report']:
     output_dir = report_config['report']['outputDirectory']
@@ -86,10 +137,11 @@ for line in fin:
 fin.close()
 fout.close()
 
-# TODO: generate the load.js 
-
 text_file = open(output_dir+"/generated.js", "w")
-text_file.write('const data=%s' % json.dumps(sorted_data, indent=2))
+if isinstance(report_config['grid']['bodyHeight'], int):
+  text_file.write("const gridBodyHeight=%s\n" % report_config['grid']['bodyHeight'])
+text_file.write('const columns=%s\n' % json.dumps(columns_data, indent=2))
+text_file.write('const data=%s\n' % json.dumps(sorted_data, indent=2))
 text_file.close()
 
 print("report generated at "+output_dir)
